80ade27a65ab810e4396c79260becb9d68f78722
Flavio Fraschetti <frasqueti@gmail.com>
2024-06-02 10:53:47 +0100

    add CurrentWheater tests

---
+++ components/__tests__/CurrentWeather.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { CurrentWeather } from '../CurrentWeather';
+
+describe('CurrentWeather', () => {
+    it('renders default correctly', () => {
+        const tree = renderer
+            .create(<CurrentWeather
+                city="London"
+                country="UK"
+                temperature="20"
+                condition="Sunny"
+                iconUrl="https://example.com/icon.png"
+            />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders future wheater prop', () => {
+        const tree = renderer
+            .create(<CurrentWeather
+                city="London"
+                time="12:00"
+                country="UK"
+                temperature="20"
+                condition="Sunny"
+                iconUrl="https://example.com/icon.png"
+                future
+            />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
